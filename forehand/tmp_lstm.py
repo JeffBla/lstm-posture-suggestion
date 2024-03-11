fefda4cd0097dace6e92f3d6bbe8a8114848d1e2
JeffBla <jeff920305@gmail.com>
2024-03-11 12:09:50 +0800

    :recycle: (lstm) Adjust/clean the code

--- forehand/tmp_lstm.py
+++ forehand/tmp_lstm.py
@@ -80,33 +80,6 @@ parser.add_argument("--prev_ckpt_path",
                     help="load previous checkpoint for further training")
 opt = parser.parse_args()
 
-# def accuracy(output, target):
-#     return (output.argmax(dim=1) == target).float().mean().item()
-
-# class CyclicLR(_LRScheduler):
-
-#     def __init__(self, optimizer, schedule, last_epoch=-1):
-#         assert callable(schedule)
-#         self.schedule = schedule
-#         super().__init__(optimizer, last_epoch)
-
-#     def get_lr(self):
-#         return [self.schedule(self.last_epoch, lr) for lr in self.base_lrs]
-
-# def cosine(t_max, eta_min=0):
-
-#     def scheduler(epoch, base_lr):
-#         t = epoch % t_max
-#         return eta_min + (base_lr - eta_min) * (1 +
-#                                                 np.cos(np.pi * t / t_max)) / 2
-
-#     return scheduler
-
-# n = 100
-# sched = cosine(n)
-# lrs = [sched(t, 1) for t in range(n * 4)]
-# plt.plot(lrs)
-
 
 class LSTMClassifier(L.LightningModule):
     """Very simple implementation of LSTM-based time-series classifier."""
@@ -120,7 +93,10 @@ class LSTMClassifier(L.LightningModule):
                            layer_dim,
                            batch_first=True,
                            bidirectional=True)
-        self.fc = nn.Linear(hidden_dim, output_dim)
+        self.fc = nn.Linear(
+            hidden_dim * 2,
+            output_dim)  # Multiply hidden_dim by 2 due to bidirectional LSTM
+        self.sigmoid = nn.Sigmoid()
 
         self.loss = None
         self.accuracy = MultilabelAccuracy(num_labels=opt.num_labels,
@@ -129,7 +105,8 @@ class LSTMClassifier(L.LightningModule):
     def forward(self, x):
         h0, c0 = self.init_hidden(x)
         out, (hn, cn) = self.rnn(x, (h0, c0))
-        out = self.fc(out[:, -1, :])
+        out = self.fc(out[:, -1, :])  # Extract the last time step output
+        out = self.sigmoid(out)
         return out
 
     def init_hidden(self, x):
@@ -142,7 +119,7 @@ class LSTMClassifier(L.LightningModule):
         self.train()
         x, y = batch
         y_hat = self(x)
-        loss = F.cross_entropy(y_hat, y)
+        loss = F.binary_cross_entropy(y_hat, y)
         self.loss = loss
         self.log('train_loss_step',
                  loss,
@@ -152,7 +129,9 @@ class LSTMClassifier(L.LightningModule):
                  logger=True)
 
         self.accuracy(y_hat, y)
-        self.log('train_acc_step', self.accuracy,on_step=True,
+        self.log('train_acc_step',
+                 self.accuracy,
+                 on_step=True,
                  on_epoch=True,
                  prog_bar=True,
                  logger=True)
@@ -160,11 +139,15 @@ class LSTMClassifier(L.LightningModule):
 
     def on_train_batch_end(self, outputs: STEP_OUTPUT, batch: int,
                            batch_idx: int) -> None:
-        self.log('train_loss_epoch', self.loss,on_step=True,
+        self.log('train_loss_epoch',
+                 self.loss,
+                 on_step=True,
                  on_epoch=True,
                  prog_bar=True,
                  logger=True)
-        self.log('train_acc_epoch', self.accuracy,on_step=True,
+        self.log('train_acc_epoch',
+                 self.accuracy,
+                 on_step=True,
                  on_epoch=True,
                  prog_bar=True,
                  logger=True)
@@ -176,7 +159,7 @@ class LSTMClassifier(L.LightningModule):
                     x, y = test_batch
                     x, y = x.to(self.device), y.to(self.device)
                     y_hat = self(x)
-                    loss = F.cross_entropy(y_hat, y)
+                    loss = F.binary_cross_entropy(y_hat, y)
                     self.log('test_loss_epoch',
                              loss,
                              on_step=True,
@@ -189,7 +172,7 @@ class LSTMClassifier(L.LightningModule):
     def test_step(self, batch, batch_idx):
         x, y = batch
         y_hat = self(x)
-        loss = F.cross_entropy(y_hat, y)
+        loss = F.binary_cross_entropy(y_hat, y)
         self.log('final_test_loss',
                  loss,
                  on_step=True,
@@ -239,6 +222,7 @@ if opt.isTrain:
 
     ################### Test #####################
     trainer.test(model, dm)
+
 else:
     ################### Predict #####################
     dm = MotionDataModule(annotations_file=opt.annotations_file,
@@ -246,62 +230,5 @@ else:
                           n_cpu=opt.n_cpu)
     model = LSTMClassifier.load_from_checkpoint(opt.prev_ckpt_path)
     trainer = L.Trainer(accelerator='auto')
-    trainer.predict(model, dm)
-
-input_dim = 10
-hidden_dim = 256
-layer_dim = 3
-output_dim = 9
-seq_dim = 128
-
-lr = 0.0005
-n_epochs = 1000
-# iterations_per_epoch = len(trn_dl)
-best_acc = 0
-patience, trials = 100, 0
-
-model = LSTMClassifier(input_dim, hidden_dim, layer_dim, output_dim)
-model = model.cuda()
-criterion = nn.CrossEntropyLoss()
-opt = torch.optim.RMSprop(model.parameters(), lr=lr)
-# sched = CyclicLR(opt, cosine(t_max=iterations_per_epoch * 2, eta_min=lr / 100))
-
-# print('Start model training')
-
-# for epoch in range(1, n_epochs + 1):
-
-#     for i, (x_batch, y_batch) in enumerate(trn_dl):
-#         model.train()
-#         x_batch = x_batch.cuda()
-#         y_batch = y_batch.cuda()
-#         sched.step()
-#         opt.zero_grad()
-#         out = model(x_batch)
-#         loss = criterion(out, y_batch)
-#         loss.backward()
-#         opt.step()
-
-#     model.eval()
-#     correct, total = 0, 0
-#     for x_val, y_val in val_dl:
-#         x_val, y_val = [t.cuda() for t in (x_val, y_val)]
-#         out = model(x_val)
-#         preds = F.log_softmax(out, dim=1).argmax(dim=1)
-#         total += y_val.size(0)
-#         correct += (preds == y_val).sum().item()
-
-#     acc = correct / total
-
-#     if epoch % 5 == 0:
-#         print(f'Epoch: {epoch:3d}. Loss: {loss.item():.4f}. Acc.: {acc:2.2%}')
-
-#     if acc > best_acc:
-#         trials = 0
-#         best_acc = acc
-#         torch.save(model.state_dict(), 'best.pth')
-#         print(f'Epoch {epoch} best model saved with accuracy: {best_acc:2.2%}')
-#     else:
-#         trials += 1
-#         if trials >= patience:
-#             print(f'Early stopping on epoch {epoch}')
-#             break
+    pred = trainer.predict(model, dm)
+    print(pred)
