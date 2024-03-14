855aa7bebeb63834ef621ffba6387a959b332f90
JeffBla <jeff920305@gmail.com>
2024-03-14 14:35:05 +0800

    :sparkles: (base model) train base model first
    Base model is trained with openpose xy info only

---
+++ forehand/base_dataPreprocess.py
@@ -0,0 +1,125 @@
+import os
+import json
+import numpy as np
+import pandas as pd
+
+from tslearn.utils import to_time_series_dataset, to_time_series
+from tslearn.preprocessing import TimeSeriesResampler, TimeSeriesScalerMeanVariance
+
+import torch
+from torch.utils.data import DataLoader, Dataset, random_split
+
+import lightning as L
+from lightning.pytorch.utilities.types import EVAL_DATALOADERS, STEP_OUTPUT, TRAIN_DATALOADERS
+
+seed = 1
+np.random.seed(seed)
+torch.manual_seed(seed)
+
+
+class MotionDataset(Dataset):
+
+    def __init__(self,
+                 openpose_files_df,
+                 angle_files_df,
+                 label_vtr_set=None,
+                 transform=None):
+        self.openpose_files_df = openpose_files_df
+        self.label_vtr_set = label_vtr_set
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.label_vtr_set)
+
+    def __getitem__(self, idx):
+        openpose_df = pd.read_csv(
+            self.openpose_files_df[idx]).drop(columns=['frame'])
+
+        if self.transform:
+            openpose_df = self.transform(openpose_df)
+
+        ts = to_time_series(openpose_df.values)
+
+        if self.label_vtr_set is None:
+            return ts
+        else:
+            return ts, self.label_vtr_set[idx]
+
+
+class MotionDataModule(L.LightningDataModule):
+
+    def __init__(self,
+                 batch_size: int = 32,
+                 annotations_file: str = None,
+                 n_cpu: int = 2):
+        super().__init__()
+        self.batch_size = batch_size
+        self.annotations_file = annotations_file
+        self.n_cpu = n_cpu
+        self.train_test_spilt = 0.7
+        # the len of ts should be the same in lstm
+        self.FRAME = 100
+
+        self.dataset = None
+        self.pred_dataset = None
+        self.train_dataset = None
+        self.test_dataset = None
+
+    def setup(self, stage):
+
+        def CheckFrameNum(df: pd.DataFrame, filename: str):
+            scale = TimeSeriesScalerMeanVariance(mu=0., std=1.)
+
+            if df['frame'].shape[0] != self.FRAME:
+                ts = to_time_series(df.values)
+                ts = TimeSeriesResampler(sz=self.FRAME).fit_transform(ts)
+                ts = scale.fit_transform(ts)
+                df = pd.DataFrame(ts[0])
+                df.to_csv(filename, index=False)
+                return df
+            else:
+                return df
+
+        if stage == 'predict':
+            annotations_df = pd.read_csv(self.annotations_file)
+            # Check the frame number
+            for (i, row) in annotations_df.iterrows():
+                openpose_df = pd.read_csv(row['openpose_files'])
+                CheckFrameNum(openpose_df)
+
+            self.pred_dataset = MotionDataset(
+                openpose_files_df=annotations_df['openpose_files'])
+
+        else:
+            annotations_df = pd.read_csv(self.annotations_file)
+
+            # Check the frame number
+            for (i, row) in annotations_df.iterrows():
+                openpose_df = pd.read_csv(row['openpose_files'])
+                CheckFrameNum(openpose_df)
+
+            self.dataset = MotionDataset(
+                openpose_files_df=annotations_df['openpose_files'],
+                label_vtr_set=annotations_df['label_vtr_set'])
+            generator = torch.Generator().manual_seed(42)
+            self.train_dataset, self.test_dataset = random_split(
+                self.dataset,
+                [self.train_test_spilt, 1 - self.train_test_spilt], generator)
+
+    def train_dataloader(self) -> TRAIN_DATALOADERS:
+        return DataLoader(self.train_dataset,
+                          batch_size=self.batch_size,
+                          shuffle=True,
+                          num_workers=self.n_cpu)
+
+    def test_dataloader(self) -> EVAL_DATALOADERS:
+        return DataLoader(self.test_dataset,
+                          batch_size=self.batch_size,
+                          shuffle=False,
+                          num_workers=self.n_cpu)
+
+    def predict_dataloader(self) -> EVAL_DATALOADERS:
+        return DataLoader(self.pred_dataset,
+                          batch_size=self.batch_size,
+                          shuffle=False,
+                          num_workers=self.n_cpu)
---
+++ forehand/lstm.py
@@ -0,0 +1,232 @@
+import argparse
+
+import numpy as np
+import torch
+from torch import nn
+from torch.nn import functional as F
+from torch.optim.lr_scheduler import CyclicLR
+
+import lightning as L
+from torchmetrics.classification import MultilabelAccuracy
+from lightning.pytorch.utilities.types import STEP_OUTPUT
+from lightning.pytorch.callbacks import ModelCheckpoint
+
+from forehand.dataPreprocess import MotionDataset, MotionDataModule
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--isTrain',
+                    action='store_true',
+                    help='does it start to training')
+parser.add_argument(
+    "--annotations_file",
+    type=str,
+    default='',
+    help=
+    "the annotations file path(with openpose csv path, angle csv path, and label)"
+)
+parser.add_argument("--n_epochs",
+                    type=int,
+                    default=100,
+                    help="number of epochs of training")
+parser.add_argument("--batch_size",
+                    type=int,
+                    default=32,
+                    help="size of the batches")
+parser.add_argument("--lr",
+                    type=float,
+                    default=0.02,
+                    help="adam: learning rate")
+parser.add_argument("--base_lr",
+                    type=int,
+                    default=1e-5,
+                    help="the base learning rate of the cyclic learning rate")
+parser.add_argument("--max_lr",
+                    type=float,
+                    default=1e-3,
+                    help="the max learning rate of the cyclic learning rate")
+parser.add_argument("--b1",
+                    type=float,
+                    default=0.5,
+                    help="adam: decay of first order momentum of gradient")
+parser.add_argument("--b2",
+                    type=float,
+                    default=0.999,
+                    help="adam: decay of first order momentum of gradient")
+parser.add_argument(
+    "--n_cpu",
+    type=int,
+    default=4,
+    help="number of cpu threads to use during batch generation")
+parser.add_argument("--sample_interval",
+                    type=int,
+                    default=400,
+                    help="interval betwen image samples")
+parser.add_argument("--num_labels",
+                    type=int,
+                    default=3,
+                    help="the number of label")
+parser.add_argument(
+    "--label_threshold",
+    type=float,
+    default=0.5,
+    help=
+    "Threshold for transforming probability to binary (0,1) predictions in label"
+)
+parser.add_argument("--prev_ckpt_path",
+                    type=str,
+                    default=None,
+                    help="load previous checkpoint for further training")
+opt = parser.parse_args()
+
+
+class LSTMClassifier(L.LightningModule):
+    """Very simple implementation of LSTM-based time-series classifier."""
+
+    def __init__(self, input_dim, hidden_dim, layer_dim, output_dim):
+        super().__init__()
+        self.hidden_dim = hidden_dim
+        self.layer_dim = layer_dim
+        self.rnn = nn.LSTM(input_dim,
+                           hidden_dim,
+                           layer_dim,
+                           batch_first=True,
+                           bidirectional=True)
+        self.fc = nn.Linear(
+            hidden_dim * 2,
+            output_dim)  # Multiply hidden_dim by 2 due to bidirectional LSTM
+        self.sigmoid = nn.Sigmoid()
+
+        self.loss = None
+        self.accuracy = MultilabelAccuracy(num_labels=opt.num_labels,
+                                           threshold=opt.label_threshold)
+
+    def forward(self, x):
+        h0, c0 = self.init_hidden(x)
+        out, (hn, cn) = self.rnn(x, (h0, c0))
+        out = self.fc(out[:, -1, :])  # Extract the last time step output
+        out = self.sigmoid(out)
+        return out
+
+    def init_hidden(self, x):
+        h0 = torch.zeros(self.layer_dim, x.size(0), self.hidden_dim)
+        c0 = torch.zeros(self.layer_dim, x.size(0), self.hidden_dim)
+        return [t.cuda() for t in (h0, c0)]
+
+    def training_step(self, batch, batch_idx):
+        # Training step
+        self.train()
+        x, y = batch
+        y_hat = self(x)
+        loss = F.binary_cross_entropy(y_hat, y)
+        self.loss = loss
+        self.log('train_loss_step',
+                 loss,
+                 on_step=True,
+                 on_epoch=True,
+                 prog_bar=True,
+                 logger=True)
+
+        self.accuracy(y_hat, y)
+        self.log('train_acc_step',
+                 self.accuracy,
+                 on_step=True,
+                 on_epoch=True,
+                 prog_bar=True,
+                 logger=True)
+        return loss
+
+    def on_train_batch_end(self, outputs: STEP_OUTPUT, batch: int,
+                           batch_idx: int) -> None:
+        self.log('train_loss_epoch',
+                 self.loss,
+                 on_step=True,
+                 on_epoch=True,
+                 prog_bar=True,
+                 logger=True)
+        self.log('train_acc_epoch',
+                 self.accuracy,
+                 on_step=True,
+                 on_epoch=True,
+                 prog_bar=True,
+                 logger=True)
+        if batch_idx % 100 == 0:
+            self.eval()
+            with torch.no_grad():
+                test_dataloader = self.dm.test_dataloader()
+                for test_batch in test_dataloader:
+                    x, y = test_batch
+                    x, y = x.to(self.device), y.to(self.device)
+                    y_hat = self(x)
+                    loss = F.binary_cross_entropy(y_hat, y)
+                    self.log('test_loss_epoch',
+                             loss,
+                             on_step=True,
+                             on_epoch=True,
+                             prog_bar=True,
+                             logger=True)
+
+        return super().on_train_batch_end(outputs, batch, batch_idx)
+
+    def test_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self(x)
+        loss = F.binary_cross_entropy(y_hat, y)
+        self.log('final_test_loss',
+                 loss,
+                 on_step=True,
+                 on_epoch=True,
+                 prog_bar=True,
+                 logger=True)
+        return loss
+
+    def predict_step(self, batch, batch_idx):
+        pred = self(batch)
+        return pred
+
+    def configure_optimizers(self):
+        # Use Adam optimizer
+        optimizer = torch.optim.Adam(self.parameters(), lr=opt.lr)
+        lr_scheduler_config = {
+            "scheduler":
+            CyclicLR(optimizer, base_lr=opt.base_lr, max_lr=opt.max_lr),
+            "interval":
+            "epoch",
+            "frequency":
+            1,
+            "motitor":
+            'val_loss'
+        }
+        return {'optimizer': optimizer, 'lr_scheduler': lr_scheduler_config}
+
+
+if opt.isTrain:
+    ################### Train #####################
+    dm = MotionDataModule(annotations_file=opt.annotations_file,
+                          batch_size=opt.batch_size,
+                          n_cpu=opt.n_cpu)
+    model = LSTMClassifier(input_dim=10,
+                           hidden_dim=256,
+                           layer_dim=3,
+                           output_dim=9)
+
+    checkpoint_callback = ModelCheckpoint(
+        save_top_k=-1,
+        monitor='train_loss',
+        filename='resnet-{epoch:02d}-{train_loss:.4f}')
+    trainer = L.Trainer(accelerator='auto',
+                        max_epochs=opt.n_epochs,
+                        callbacks=[checkpoint_callback])
+    trainer.fit(model, dm)
+
+    ################### Test #####################
+    trainer.test(model, dm)
+
+else:
+    ################### Predict #####################
+    dm = MotionDataModule(annotations_file=opt.annotations_file,
+                          batch_size=opt.batch_size,
+                          n_cpu=opt.n_cpu)
+    model = LSTMClassifier.load_from_checkpoint(opt.prev_ckpt_path)
+    trainer = L.Trainer(accelerator='auto')
+    pred = trainer.predict(model, dm)
+    print(pred)
